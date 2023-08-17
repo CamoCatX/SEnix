7bb2101cc886b59275d3f628ab1e3db62c991601
CamoCatX <69330490+CamoCatX@users.noreply.github.com>
2023-08-16 19:17:16 -0500

    Update sway.nix

--- sway.nix
+++ sway.nix
@@ -30,6 +30,7 @@
       export QT_QPA_PLATFORM=wayland
       export CLUTTER_BACKEND=wayland
       export SDL_VIDEODRIVER=wayland
+      export MOZ_ENABLE_WAYLAND = "1"; 
     '';
 
     config = {
