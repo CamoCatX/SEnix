69d082df94050c5a55ce541a1139a73f51e47200
CamoCatX <69330490+CamoCatX@users.noreply.github.com>
2023-08-18 13:42:21 -0500

    Update kernel.nix

--- kernel.nix
+++ kernel.nix
@@ -1,10 +1,27 @@
 { config, pkgs, lib, ... }:
 {
    boot.kernelPackages = pkgs.linuxPackages_hardened;
+
    # Prevent replacing the running kernel w/o reboot
    security.protectKernelImage = true;
    #security.lockKernelModules = true;
    security.virtualisation.flushL1DataCache = "always";
    security.sudo.execWheelOnly = true;
    security.forcePageTableIsolation = true;
+   boot.kernelParams = [
+     # Slab/slub sanity checks, redzoning, and poisoning
+     "slub_debug=FZP"
+
+     # Overwrite free'd memory
+     "page_poison=1"
+
+     # Enable page allocator randomization
+     "page_alloc.shuffle=1"
+   ];
+   boot.blacklistedKernelModules = [
+     # Obscure network protocols
+     "ax25"
+     "netrom"
+     "rose"
+   ];
 }
