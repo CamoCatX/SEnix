9e6c78cc5b29ba2207c8d2779cdee73178d0def7
CamoCatX <69330490+CamoCatX@users.noreply.github.com>
2023-08-18 14:20:29 -0500

    Update sound.nix

--- sound.nix
+++ sound.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 {
-# Enable sound with Pipewire.
+  # Enable sound with Pipewire.
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -12,4 +12,24 @@
   environment.systemPackages = with pkgs; [
     helvum
   ];
+  environment.etc = {
+	  "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+		  bluez_monitor.properties = {
+			  ["bluez5.enable-sbc-xq"] = true,
+			  ["bluez5.enable-msbc"] = true,
+			  ["bluez5.enable-hw-volume"] = true,
+		  	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  		}
+	  '';
+  };
+  environment.etc = {
+	  "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+		  bluez_monitor.properties = {
+		  	["bluez5.enable-sbc-xq"] = true,
+		  	["bluez5.enable-msbc"] = true,
+		  	["bluez5.enable-hw-volume"] = true,
+		  	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+		  }
+  	'';
+  };
 }
