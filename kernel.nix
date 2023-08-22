315f69ac4c6ba7f3c8e0ef3cda48f830cfe0df76
CamoCatX <69330490+CamoCatX@users.noreply.github.com>
2023-08-22 14:59:00 -0500

    Update kernel.nix

--- kernel.nix
+++ kernel.nix
@@ -4,7 +4,7 @@
 
    # Prevent replacing the running kernel w/o reboot
    security.protectKernelImage = true;
-   #security.lockKernelModules = true;
+   security.lockKernelModules = true;
    security.virtualisation.flushL1DataCache = "always";
    security.sudo.execWheelOnly = true;
    security.forcePageTableIsolation = true;
