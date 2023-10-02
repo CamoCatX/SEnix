925c01bacd9463a04143b2fa0243804a9cba02b5
CamoCatX <69330490+CamoCatX@users.noreply.github.com>
2023-10-01 20:45:19 -0500

    Update boot.nix

--- boot.nix
+++ boot.nix
@@ -13,4 +13,32 @@
     useTmpfs = lib.mkDefault true;
     cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
   };
+
+  security.polkit.enable = true;
+  systemd = {
+  user.services.polkit-authentication-agent-1 = {
+    description = "polkit-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit}/libexec/polkit-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+        # Sandboxing features
+        PrivateTmp=yes
+        NoNewPrivileges=true
+        ProtectSystem=strict
+        ProtectKernelTunables=yes
+        ProtectKernelModules=yes
+        ProtectControlGroups=yes
+        PrivateDevices=yes
+      };
+  };
+   extraConfig = ''
+     DefaultTimeoutStopSec=10s
+   '';
+
 }
